a1b977e08f11b0df6d1b058865bcb884f9bfb60f
Darren <nardol.darren@gmail.com>
2024-05-18 18:59:19 +0200

    add nav, bootstrap and scss

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import NavBar from "./Components/Nav/Nav";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <NavBar></NavBar>
+      </header>
     </div>
   );
 }
---
+++ src/Components/Nav/Nav.js
@@ -0,0 +1,54 @@
+export default function NavBar() {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="Accueil">
+          <img className="logo" src="Img/Logo.png" alt=""></img>
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <Nav></Nav>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+function Nav() {
+  return (
+    <ul className="navbar-nav">
+      <li className="nav-item">
+        <a className="nav-link active" aria-current="page" href="Bâtiment">
+          Bâtiment
+        </a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="Services">
+          Service
+        </a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="Fabrication">
+          Fabrication
+        </a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link " href="Alimentation">
+          Alimentation
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+/* function SearchBar() {} */
